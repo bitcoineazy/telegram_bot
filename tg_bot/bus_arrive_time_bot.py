6de7712e07f037cbae1ed00d0afcd33775fe542d
bitcoineazy <bitcoineazy@gmail.com>
2021-03-30 01:07:33 +0300

    Bus stops - API requests to get List of arrive and departure times - done

--- tg_bot/bus_arrive_time_bot.py
+++ tg_bot/bus_arrive_time_bot.py
@@ -3,76 +3,67 @@
 import os
 import time
 
-import logging
 import requests
 import telegram
 from dotenv import load_dotenv
 
 load_dotenv()
 
-PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
+YANDEX_TOKEN = os.getenv('YANDEX_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+URL = 'https://api.rasp.yandex.net/v3.0/search/'
+HEADERS = {'Authorization': f'OAuth {YANDEX_TOKEN}'}
+STATIONS = [os.getenv('HOME_STATION'), os.getenv('SUB_STATION')]
 
 
 def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
-    if homework_name is None:
-        return 'Пришли пустые данные homework_name'
-    homework_status = homework.get('status')
-    if homework_status is None:
-        return 'Пришли пустые данные homework_status'
-    if homework_status != 'approved':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = ('Ревьюеру всё понравилось,'
-                   ' можно приступать к следующему уроку.')
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
-def get_homework_statuses(current_timestamp):
+
+def get_arrive_list():
     try:
-        homework_statuses = requests.get(
-            URL, headers=HEADERS, params={
-                'from_date': current_timestamp
-                if current_timestamp is not None
-                else int(time.time())})
-        return homework_statuses.json()
+        arrive_list = requests.get(URL, params={
+            'apikey': YANDEX_TOKEN,
+            'from': STATIONS[0],
+            'to': STATIONS[1],
+            'transport_types': 'bus',
+            'limit': 10,
+        })
+        return arrive_list.json()
     except Exception as e:
-        error_message = f'Бот столкнулся с ошибкой: {e}, Лог: {logging.ERROR}'
-        logging.error(error_message)
+        error_message = f'Бот столкнулся с ошибкой: {e}'
         time.sleep(5)
 
 
 def send_message(message, bot_client):
-    logging.info(f'Sent message: {message}, to user: {CHAT_ID}')
-    return bot_client.send_message(CHAT_ID, message)
-
+    #logging.info(f'Sent message: {message}, to user: {CHAT_ID}')
+    #return bot_client.send_message(CHAT_ID, message)
+    pass
 
 def main():
-    bot_start_debug_log = logging.getLogger('BotStart')
-    bot_start_debug_log.setLevel(logging.DEBUG)
     bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())  # начальное значение timestamp
 
     while True:
         try:
-            new_homework = get_homework_statuses(current_timestamp)
-            if new_homework.get('homeworks'):
+            all_schedule = get_arrive_list()
+            for i in range(5):
+                print(all_schedule['segments'][i].get('arrival'), f'arrival {i}')
+                print(all_schedule['segments'][i].get('departure'), f'departure {i}')
+            '''if new_homework.get('homeworks'):
                 send_message(
                     parse_homework_status(
                         new_homework.get('homeworks')[0]),
                     bot_client)
             current_timestamp = new_homework.get(
                 'current_date', current_timestamp)  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в 5 минут
-
+            time.sleep(300)  # опрашивать раз в 5 минут'''
+            time.sleep(300)
         except Exception as e:
-            error_message = (f'Бот столкнулся с ошибкой: {e},'
-                             f' Лог: {logging.ERROR}')
-            send_message(error_message, bot_client)
+            error_message = f'Бот столкнулся с ошибкой: {e},'
+            #send_message(error_message, bot_client)
             time.sleep(5)
 
 
