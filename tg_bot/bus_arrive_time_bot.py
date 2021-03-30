695d433ee5280759f3c406f570a8bcf2c1b091ba
bitcoineazy <bitcoineazy@gmail.com>
2021-03-30 23:17:29 +0300

    Updated bot with CommandHandler for my stations

--- tg_bot/bus_arrive_time_bot.py
+++ tg_bot/bus_arrive_time_bot.py
@@ -5,53 +5,65 @@ import time
 
 import requests
 import telegram
+from telegram.ext import Updater, CommandHandler
 from dotenv import load_dotenv
 
 load_dotenv()
 
 YANDEX_TOKEN = os.getenv('YANDEX_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN_BUS')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 URL = 'https://api.rasp.yandex.net/v3.0/search/'
 HEADERS = {'Authorization': f'OAuth {YANDEX_TOKEN}'}
 STATIONS = [os.getenv('HOME_STATION'), os.getenv('SUB_STATION')]
 
 
-def parse_homework_status(homework):
-    homework_name = homework.get('homework_name')
+def parse_all_routes_list(all_routes):
 
+    parsed_routes = all_routes
 
 
 def get_arrive_list():
+    all_routes = {}  # Все маршруты в нотации время отправления : номер маршрута
+    time_get = time.time()
+    time_iso8601 = time.ctime(time_get)
+    print(time_iso8601)
     try:
+        print()
         arrive_list = requests.get(URL, params={
             'apikey': YANDEX_TOKEN,
             'from': STATIONS[0],
             'to': STATIONS[1],
             'transport_types': 'bus',
-            'limit': 10,
+            'limit': 500,
         })
-        return arrive_list.json()
+        arrive_list_json = arrive_list.json()
+        for i in range(len(arrive_list_json.get('segments'))):
+            all_routes.update(
+                {arrive_list_json.get('segments')[i].get('departure'):
+                 arrive_list_json.get('segments')[i].get('thread').get('number')})
+        return all_routes
     except Exception as e:
         error_message = f'Бот столкнулся с ошибкой: {e}'
         time.sleep(5)
 
 
 def send_message(message, bot_client):
-    #logging.info(f'Sent message: {message}, to user: {CHAT_ID}')
-    #return bot_client.send_message(CHAT_ID, message)
+    # logging.info(f'Sent message: {message}, to user: {CHAT_ID}')
+    # return bot_client.send_message(CHAT_ID, message)
     pass
 
+
 def main():
-    bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
+
+    bot_client = Updater(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())  # начальное значение timestamp
 
     while True:
         try:
-            all_schedule = get_arrive_list()
-            for i in range(5):
-                print(all_schedule['segments'][i].get('arrival'), f'arrival {i}')
-                print(all_schedule['segments'][i].get('departure'), f'departure {i}')
+            all_routes = get_arrive_list()
+            print(all_routes)
+
             '''if new_homework.get('homeworks'):
                 send_message(
                     parse_homework_status(
@@ -63,7 +75,7 @@ def main():
             time.sleep(300)
         except Exception as e:
             error_message = f'Бот столкнулся с ошибкой: {e},'
-            #send_message(error_message, bot_client)
+            # send_message(error_message, bot_client)
             time.sleep(5)
 
 
